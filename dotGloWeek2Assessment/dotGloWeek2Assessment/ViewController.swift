1586d5c36fbd8e66a7e16a553391e7cf9938a18d
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-10 15:19:07 -0700

    Initial commit.  Adds starter project to repo.

---
+++ dotGloWeek2Assessment/dotGloWeek2Assessment/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  dotGloWeek2Assessment
+//
+//  Created by Jake Gloschat on 2/10/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

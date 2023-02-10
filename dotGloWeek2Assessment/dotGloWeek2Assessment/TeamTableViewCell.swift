8e4ff678cac998de6a3450551a4dd795e7b20c39
Jake Gloschat <handz@Jakes-Mac-mini.local>
2023-02-10 15:42:40 -0700

    sets up custom cell on storyboard

---
+++ dotGloWeek2Assessment/dotGloWeek2Assessment/TeamTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  TeamTableViewCell.swift
+//  dotGloWeek2Assessment
+//
+//  Created by Jake Gloschat on 2/10/23.
+//
+
+import UIKit
+
+class TeamTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
